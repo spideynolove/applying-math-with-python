eefa3570a53c02b792afc87ca07591c82f73b6b8
spideynolove <spideynolove@gmail.com>
2024-06-23 01:16:50 +0700

    feat: Add support for generating numbers with floating point precision
    The `generate_numbers` function now accepts an additional parameter `etype` to specify whether the generated numbers should be of type 'float' or 'int'. If `etype` is set to 'float', the selected elements will be rounded to the specified precision using random.uniform(). If `etype` is set to 'int', the selected elements will be converted to integers. This enhancement allows for more flexibility in generating numbers with different data types.

--- materials/functions.py
+++ materials/functions.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 
 
-def generate_numbers(range_=144, sub_=13, change_sign_count=5):
+def generate_numbers(range_=144, sub_=13, change_sign_count=5, etype='int', precision=1):
     a = [i for i in range(range_)]
     random.shuffle(a)
     selected_elements = random.sample(a, sub_)
@@ -10,6 +10,14 @@ def generate_numbers(range_=144, sub_=13, change_sign_count=5):
     indices_to_change = random.sample(range(sub_), change_sign_count)
     for idx in indices_to_change:
         selected_elements[idx] = -selected_elements[idx]
+
+    if etype == 'float':
+        selected_elements = [round(float(num) + random.uniform(-0.5, 0.5), precision) for num in selected_elements]
+    elif etype == 'int':
+        selected_elements = [int(num) for num in selected_elements]
+    else:
+        raise ValueError("etype must be 'float' or 'int'")
+
     return selected_elements
 
 
